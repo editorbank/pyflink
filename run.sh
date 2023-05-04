d8bc77bc980206c7720099fa5c2b49612fa87d19
editornank <>
2023-05-04 13:29:32 +0300

    add params

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!
-docker run -it --rm -p 8081:8081 -p 8888:8888 --name pyflink editorbank/pyflink jupyterlab
+docker run -it --rm -p 8081:8081 -p 8888:8888 --name pyflink editorbank/pyflink jupyterlab $@
